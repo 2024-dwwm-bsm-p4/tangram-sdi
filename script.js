668bde19e05ffe8ad417b7823c0ecf410e4f20ba
sachadrmn <sacha.diereman@gmail.com>
2024-09-27 10:08:33 +0200

    dark-mode corrigé

--- script.js
+++ script.js
@@ -5,30 +5,3 @@ const body = document.body;
 function toggleDarkMode() {
     body.classList.toggle('dark-mode');
 }
-
-document.querySelector('.figure1').addEventListener('click',function() {
-    if (!document.querySelector('link[href="square.css"]')) {
-        const link = document.createElement('link');
-        link.rel = "stylesheet";
-        link.href = "square.css";
-        document.head.appendChild(link);
-    }
-});
-
-document.querySelector('.figure2').addEventListener('click',function() {
-    if (!document.querySelector('link[href="boat.css"]')) {
-        const link = document.createElement('link');
-        link.rel = "stylesheet";
-        link.href = "boat.css";
-        document.head.appendChild(link);
-    }
-});
-
-document.querySelector('.figure3').addEventListener('click',function() {
-    if (!document.querySelector('link[href="duck.css"]')) {
-        const link = document.createElement('link');
-        link.rel = "stylesheet";
-        link.href = "duck.css";
-        document.head.appendChild(link);
-    }
-});
